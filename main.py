fb726a49479a654748a1764b553dae05e127dc6a
Tim <tmwong031@gmail.com>
2025-05-11 16:50:49 +0800

    Reset the function

--- main.py
+++ main.py
@@ -2,16 +2,17 @@ import bs4
 from urllib.request import urlopen
 import json
 
-api_url = "https://api.data.gov.hk/v1/carpark-info-vacancy?data=vacancy&vehicleTypes=privateCar&lang=en_US"
+the_url = "https://api.data.gov.hk/v1/carpark-info-vacancy?data=vacancy&vehicleTypes=privateCar&lang=en_US"
 
-def get_response_data(url):
+def get_response_data(input_url):
     try:
         # https://api.data.gov.hk/v1/carpark-info-vacancy?data=<param>&vehicleTypes=<param>&carparkIds=<param>&extent=<param>&lang=<param>
         # data = "info" or "vacancy"
         # vehicleTypes = "privateCar", "LGV", "HGV", "CV", "coach", "motorCycle"
         # lang = "en_US", "zh_HK", "zh_CN"
         # Get response from the Carpark Vacancy API
-        with urlopen(url) as response:
+
+        with urlopen(input_url) as response:
             response_data = response.read().decode("utf-8")
             response_data = json.loads(response_data)  # Parse the response data
 
@@ -22,7 +23,7 @@ def get_response_data(url):
     return response_data
 
 def main():
-    vacancy_data = get_response_data(api_url)["result"]
+    vacancy_data = get_response_data(the_url)["results"]
     print(f"Response data: {vacancy_data}")
 
 
